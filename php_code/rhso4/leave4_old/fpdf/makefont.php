af8a2d2e8f4743313b6cc4a2f0c5cbf88c6f3b44
tumzaaa2009 <tumzaaa_@hotmail.com>
2025-01-08 09:48:18 +0700

    Add Rule id Ddos

--- php_code/rhso4/leave4_old/fpdf/makefont.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('makefont/makefont.php'); // แทรกไฟล์สร้างฟอนต์จากโฟลเดอร์ makefont มาใช้งาน
-
-MakeFont('D:\xampp\htdocs\r8_backoffice\fpdf\font\THSarabun.ttf','cp874'); // MakeFont('พาธของฟอนต์','Encode ของฟอนต์');
-MakeFont('D:\xampp\htdocs\r8_backoffice\fpdf\font\THSarabun Bold.ttf','cp874'); // MakeFont('พาธของฟอนต์','Encode ของฟอนต์');
-MakeFont('D:\xampp\htdocs\r8_backoffice\fpdf\font\THSarabun BoldItalic.ttf','cp874'); // MakeFont('พาธของฟอนต์','Encode ของฟอนต์');
-MakeFont('D:\xampp\htdocs\r8_backoffice\fpdf\font\THSarabun Italic.ttf','cp874'); // MakeFont('พาธของฟอนต์','Encode ของฟอนต์');
-
-?>
